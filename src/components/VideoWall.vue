d700b7345139ec4d5f22c1ee325aa1aeb30044fe
bulb <pumpkinjack@livemail.tw>
2020-11-17 21:47:01 +0800

    範例改寫為ts

---
+++ src/components/VideoWall.vue
@@ -0,0 +1,35 @@
+<template>
+  <section data-name="A01Reactive">
+    <button @click="increment">
+      Count is: {{ state.count }}, double is: {{ state.double }}
+    </button>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, computed } from 'vue'
+
+interface MyNum {
+  count: number,
+  double: any
+}
+
+export default defineComponent({
+  name: 'VideoWall',
+  setup () {
+    const state: any = reactive<MyNum>({
+      count: 0,
+      double: computed(() => state.count * 2)
+    })
+
+    function increment () {
+      state.count += 1
+    }
+
+    return {
+      state,
+      increment
+    }
+  }
+})
+</script>
